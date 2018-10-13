18a64eaf168d7e8283f5ad0d930d3ed11ba69dd9
baibing <baibing@rongcloud.cn>
2018-10-13 18:09:38 +0800

    insert default super admin account

--- database/sql/V1__DDL_create_tables.sql
+++ database/sql/V1__DDL_create_tables.sql
@@ -1,36 +1,72 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
 -- -----------------------------------------------------
 -- Table `t_user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `t_user` (
-  `username` VARCHAR(32) NOT NULL COMMENT '登录用户名',
-  `password` VARCHAR(64) NULL COMMENT '登录密码',
-  `create_dt` DATETIME NULL COMMENT '创建日期',
-  `update_dt` DATETIME NULL COMMENT '更新日期',
-  `pwdsalt` VARCHAR(36) NULL COMMENT 'Hash密码用的盐',
-  `last_login_dt` DATETIME NULL,
-  `_uid` VARCHAR(32) NOT NULL COMMENT 'uuid',
-  PRIMARY KEY (`_uid`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` VARCHAR(36) NOT NULL COMMENT 'uuid',
+  `username` VARCHAR(32) NOT NULL COMMENT 'login username',
+  `password` VARCHAR(64) NULL COMMENT 'login password',
+  `create_dt` DATETIME NULL COMMENT 'create date',
+  `update_dt` DATETIME NULL COMMENT 'update date',
+  `pwdsalt` VARCHAR(36) NULL COMMENT 'Hash salt',
+  `last_login_dt` DATETIME NULL COMMENT 'last login time',
+  `name` VARCHAR(16) NULL COMMENT 'username',
+  `mobile` VARCHAR(16) NULL COMMENT 'mobile',
+  `type` int(4) NOT NULL DEFAULT '0' COMMENT '默认0为老师，1为客户, 2为超管',
+  `state` int(4) NOT NULL DEFAULT '0' COMMENT '默认0为激活，1为去激活，2为删除',
+  `keyword` TEXT NULL COMMENT 'keyword',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `t_classes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `t_classes` (
-  `_uid` VARCHAR(36) NOT NULL COMMENT "uuid",
-  `type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '类型',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` VARCHAR(32) NOT NULL COMMENT 'uuid',
+  `type` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'type',
   `create_dt` datetime DEFAULT NULL,
-  `update_dt` datetime DEFAULT NULL
+  `update_dt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `t_user_role` (
+  `id` bigint(20) NOT NULL,
+  `userid` int(11) NOT NULL COMMENT '用户ID',
+  `roleid` bigint(20)  NOT NULL COMMENT '角色ID',
+  `is_deleted` int(2) NOT NULL DEFAULT '0' COMMENT '默认0为可用，1为删除',
+  `create_dt` datetime(3) DEFAULT NULL,
+  `update_dt` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户-角色关系表';
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `t_role_permission` (
+  `id` bigint(20) NOT NULL,
+  `roleid` bigint(20) NOT NULL COMMENT '角色ID',
+  `permid` bigint(20) NOT NULL COMMENT '权限ID',
+  `is_deleted` int(2) NOT NULL DEFAULT '0' COMMENT '默认0位可用，1为删除',
+  `create_dt` datetime(3) DEFAULT NULL,
+  `update_dt` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色-权限关系表';
+
+CREATE TABLE `t_role` (
+  `id` bigint(20) NOT NULL,
+  `role_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '角色名称',
+  `descinfo` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '描述',
+  `is_deleted` int(2) NOT NULL DEFAULT '0' COMMENT '默认0为可用，1为删除',
+  `create_dt` datetime(3) DEFAULT NULL,
+  `update_dt` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色表';
+
+CREATE TABLE `t_permission` (
+  `id` bigint(20) NOT NULL,
+  `perm_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '权限名称',
+  `descinfo` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '描述',
+  `is_deleted` int(2) NOT NULL DEFAULT '0' COMMENT '默认0可用, 1为删除',
+  `create_dt` datetime(3) DEFAULT NULL,
+  `update_dt` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='权限表';
